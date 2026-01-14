ffdb4a16f4ec5f94598955c0da871e44a317b503
Christyan Shindy <christyan@sattvaconsultoria.com.br>
2026-01-14 14:38:22 -0300

    inserido lógica do jogo

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,22 +1,45 @@
 import { useState } from 'react'
 import styles from './Game.module.css'
 import GameOption from '../gameOption/GameOption'
-
+import Icon from '../icon/Icon'
 function Game () {
 
+  const [currentPlayear, setCurrentPlayer] = useState(1)
   const [gameState, setGameState] = useState(Array(9).fill(0))
 
+  const hadleClick = (pos) => {
+    if(gameState[pos] === 0){
+      let newGameState = [...gameState]
+      newGameState[pos] = currentPlayear
+      setCurrentPlayer(currentPlayear * -1)
+      setGameState(newGameState)
+    }
+  }
+
   return (
-  <div className={styles.game}>
+    <div className={styles.gameContent}>
+      <div className={styles.game}>
     {
       gameState.map((value, pos) => 
         <GameOption 
           key={`game-option-pos-${pos}`}
           status={value}
+          onClick={()=> hadleClick(pos)}
         />)
-
     }
-  </div> 
+      </div> 
+      <div className={styles.gameInfo}>
+        <h4>
+          Próximo a jogar:
+        </h4>
+        {
+          currentPlayear === 1 && <Icon iconName="circle"/>
+        }
+        {
+          currentPlayear === -1 && <Icon iconName="x"/>
+        }
+      </div>
+    </div>
   )
 }
 
